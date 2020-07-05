a5b045c881b0477a2090915d38da330c50915fb0
Jouleslabs <muhin.cse.diu@gmail.com>
2020-07-05 23:48:32 +0600

    added code

--- includes/class-min-max-for-woocommerce.php
+++ includes/class-min-max-for-woocommerce.php
@@ -154,8 +154,10 @@ class Min_Max_For_Woocommerce {
 
 		$plugin_admin = new Min_Max_For_Woocommerce_Admin( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
-		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'mmfwc_enqueue_styles' );
+		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'mmfwc_enqueue_scripts' );
+		$this->loader->add_action( 'add_meta_boxes', $plugin_admin, 'mmfwc_meta_box_create' );
+		$this->loader->add_action( 'save_post', $plugin_admin, 'mmfwc_save_meta_box' );
 
 	}
 
@@ -172,6 +174,12 @@ class Min_Max_For_Woocommerce {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		/*Function to manipulate custom minimum and maximum purchase*/
+		$this->loader->add_filter('woocommerce_quantity_input_args',  $plugin_public, 'wc_mmax_quantity_input_args', 10, 2 );
+		/*Function to check weather the maximum quantity is already existing in the cart*/
+		$this->loader->add_action('woocommerce_add_to_cart',  $plugin_public, 'wc_mmax_custom_add_to_cart',10,2 );
+		#filter hook to remove extra add to cart button in the shop and category pages
+		$this->loader->add_filter('woocommerce_loop_add_to_cart_link',  $plugin_public, '_wcmmax_add2cart' );
 
 	}
 
--- public/class-min-max-for-woocommerce-public.php
+++ public/class-min-max-for-woocommerce-public.php
@@ -60,9 +60,7 @@ class Min_Max_For_Woocommerce_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles() {
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/min-max-for-woocommerce-public.css', array(), $this->version, 'all' );
-
 	}
 
 	/**
@@ -71,9 +69,97 @@ class Min_Max_For_Woocommerce_Public {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/min-max-for-woocommerce-public.js', array( 'jquery' ), $this->version, false );
+	}
+
+
+	public function wc_mmax_quantity_input_args( $args, $product ) {
+		
+		if( function_exists('icl_object_id') ) {
+			$default_language = wpml_get_default_language();
+			$prod_id = icl_object_id( $product->get_id(), 'product', true, $default_language );
+		} else {
+			$prod_id = $product->get_id();
+		}
+
+		$mmaxEnable = get_post_meta( $prod_id, '_mmfwc_prd_opt_enable', true );
+		$minQty     = get_post_meta( $prod_id, '_mmfwc_min', true );
+		$maxQty     = get_post_meta( $prod_id, '_mmfwc_max', true );
+		
+		if ( $minQty > 0 && $maxQty > 0 && $mmaxEnable == 1 ) {
+			$args['min_value'] = $minQty; // Starting value
+			$args['max_value'] = $maxQty; // Ending value
+		}
+
+		return $args;
+	}
+
+	public function wc_mmax_custom_add_to_cart( $args,$product ) {
+
+		$orderQTY = $_POST['quantity'];
+		$mmaxEnable = get_post_meta($product, '_mmfwc_prd_opt_enable', true);
+		$minQty     = get_post_meta($product, '_mmfwc_min', true);
+		$maxQty     = get_post_meta($product, '_mmfwc_max', true);
+		$cartQty =  wc_mmax_woo_in_cart($product);
+		if(get_option('_wcmmax_options_option_name') != NULL && get_option('_wcmmax_options_option_name') !=''){
+			$maxQTYMsg = get_option('_wcmmax_options_option_name');
+		}else {
+			$maxQTYMsg = 'You have already added the maximum Quantity for the product for the current purchase';
+		}
+
+		if( $maxQty < $cartQty && $mmaxEnable == 1 ) {
+			wc_add_notice( $maxQTYMsg, 'error' );
+			exit( wp_redirect( get_permalink($product) ) );
+		}
+
+		if( ($orderQTY + $cartQty)  < $minQty && $mmaxEnable == 1 ) {
+			wc_add_notice(__( 'You have ordered '.$orderQTY.'  which is less than the allowed Minimum Quantity '.$minQty.'','min-max-for-woocommerce'),'error' );
+			exit( wp_redirect( get_permalink($product) ) );
+		}
 
 	}
+
+	public function wc_mmax_woo_in_cart( $product_id ) {
+		global $woocommerce;
+		foreach($woocommerce->cart->get_cart() as $key => $val ) {
+		
+			$_product = $val['data'];
+			if($product_id == $_product->get_id()) {
+
+			
+			return  $val['quantity'];
+		
+			}
+		}
+	
+		return 0;
+	}
+
+	public function _wcmmax_add2cart( $link ) {
+		global $product;
+		$product_id = $product->id;
+		$product_sku = $product->get_sku();
+		$product_type = $product->get_type();
+		$qtylink = ''; 
+			$mmaxEnable = get_post_meta($product_id, '_wc_mmax_prd_opt_enable', true);
+			$minQty     = get_post_meta($product_id, '_wc_mmax_min', true);
+		
+		if($product_type !='simple' && $mmaxEnable == 1){
+			$qtylink = '&quantity='.$minQty;
+		} 
+		$ajax_cart_en = 'yes' === get_option( 'woocommerce_enable_ajax_add_to_cart' );
+			if ($ajax_cart_en &&  $mmaxEnable == 0) { $ajax_class = 'ajax_add_to_cart'; }
+		$link = sprintf( '<a href="%s" rel="nofollow" data-product_id="%s" data-product_sku="%s" data-quantity="%s" class="button %s product_type_%s %s">%s</a>',
+				esc_url( $product->add_to_cart_url().$qtylink ),
+				esc_attr( $product->id ),
+				esc_attr( $product->get_sku() ),
+				esc_attr( isset( $minQty ) ? $minQty : 1 ),
+				$product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
+				esc_attr( $product->product_type ),
+				esc_attr( $ajax_class ),
+				esc_html( $product->add_to_cart_text() )
+			);
+		return $link;
+	}	
 
 }
